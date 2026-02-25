297afd0c405a73b748cb14d3e647ccc9bddbcb06
judel fintch <judel@gmail.com>
2026-02-25 10:33:38 +0200

    Block APP_DEBUG in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production') && config('app.debug')) {
+            throw new \RuntimeException('APP_DEBUG must be false in production.');
+        }
     }
 }
